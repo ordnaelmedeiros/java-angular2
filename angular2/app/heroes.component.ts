e5bd04f8db65792a59c90a15e77a5e6655d4a40d
ordnaelmedeiros <lrmarkanjo@gmail.com>
2016-07-25 20:41:08 -0300

    Rotas

---
+++ angular2/app/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+@Component({
+  selector: 'my-heroes',
+  templateUrl: 'app/heroes.component.html',
+  styleUrls:  ['app/heroes.component.css'],
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
+  getHeroes() {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero) { this.selectedHero = hero; }
+  gotoDetail() {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
+
